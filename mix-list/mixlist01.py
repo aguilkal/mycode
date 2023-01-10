54f90a5b10f42197d85665ff1784dcfb2f892f70
aguilkal <ajaguilar22@gmail.com>
2023-01-10 17:35:32 +0000

    mixed lists

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -15,3 +15,6 @@ print("The last item in the list (state): " + my_list[2] )
 iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
 
 print("IP Addresses: " +iplist[3]+ " and " +iplist[4])
+
+# example 3 - use an 'f-string' I like this a little better
+print(f"IP addresses: {iplist[3]}, and {iplist[4]}")
