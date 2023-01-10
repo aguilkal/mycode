02613272f58d465288672ea520fa5b667229fc11
aguilkal <ajaguilar22@gmail.com>
2023-01-10 16:36:48 +0000

    join and split

---
+++ hello_name.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def hello():
+    name = input("What is your first name")
+
+    day = input("What day of the week is it")
+
+    print("Hello, " +name+ "! Happy " +day+ "!")
+
+hello()    
