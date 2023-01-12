38f7300bcf0fc76312c3cebb2d2c112db9ed22a1
aguilkal <ajaguilar22@gmail.com>
2023-01-12 05:44:19 +0000

    mini project

--- mini_project/rock_paper_scissors.py
+++ mini_project/rock_paper_scissors.py
@@ -1,51 +1,52 @@
 #!/usr/bin/python3
 
 import random
-#print rock paper scissors to screen and save user input
-print("Lets play Rock, Paper, Scissors,")
-player = input("--> ")
-
-#convert input to lowercase
-player.lower()
 
 #create a list to choose from
-
 rps = ["rock", "paper", "scissors"]
 
-#generate a random number 0 -2
-ran = random.randrange(3)
+while True:
+    #print rock paper scissors to screen and save user input. Convert it to lowercase
+    print("!!! Lets play Rock, Paper, Scissors !!!")
+    print('Enter ("rock", "paper", "scissors")')
+    player = input("--> ").lower()
 
-#set the random number to the index of rps and save as variable
 
-computer = rps[ran]
+    #generate a random number 0 -2
+    ran = random.randrange(3)
 
-#if else game logic
-print(f"Boom!...Your choice: {player}...Computer's choice: {computer}")
+    #set the random number to the index of rps and save as variable
+    computer = rps[ran]
 
-if player == "rock" and computer == "paper":
-    print("You lose!")
+    #if else game logic
+    print(f"Boom!...Your choice: {player}...Computer's choice: {computer}")
+ 
+    if player == computer:
+        print("It's a Tie!")
 
-elif player == "rock" and computer == "scissors":
-    print("You Win!")
+    elif player == "rock" and computer == "paper":
+        print("You lose!")
 
-elif player == "rock" and computer == "rock":
-    print("Tie!")
+    elif player == "rock" and computer == "scissors":
+        print("You Win!")
 
-elif player == "paper" and computer == "paper":
-    print("Tie!")
+    elif player == "paper" and computer == "scissors":
+        print("You Lose!")
 
-elif player == "paper" and computer == "scissors":
-    print("You Lose!")
+    elif player == "paper" and computer == "rock":
+        print("You Win!")
 
-elif player == "paper" and computer == "rock":
-    print("You Win!")
-elif player == "scissors" and computer == "paper":
-    print("You Win!")
+    elif player == "scissors" and computer == "paper":
+        print("You Win!")
 
-elif player == "paper" and computer == "scissors":
-    print("You Lose!")
+    elif player == "scissors" and computer == "rock":
+        print("You Lose!")
 
-elif player == "paper" and computer == "paper":
-    print("Tie!")
-else:
-    print("Invalid choice...please choose rock, paper, scissors")
+    else:
+        print("Invalid choice...please choose rock, paper, scissors")
+
+    print("Would you like to play again? Y/N")
+    exit = input("-->").upper()
+    print(exit)
+    if exit == "N":
+        break
