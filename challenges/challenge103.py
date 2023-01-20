ff392d7faad27799bc4b1c1b5762490fe977beac
aguilkal <ajaguilar22@gmail.com>
2023-01-20 05:33:47 +0000

    finally got 103 working

--- challenges/challenge103.py
+++ challenges/challenge103.py
@@ -7,10 +7,24 @@ from flask import render_template
 
 app= Flask(__name__)
 
-URL= "https://opentdb.com/api.php?amount=3&category=11&difficulty=medium&type=multiple"
+##URL= "https://opentdb.com/api.php?amount=3&category=11&difficulty=medium&type=multiple"
 
-resp= requests.get(URL).json()
+##resp= request.get(URL).json()
 
 @app.route("/")
 def index():
-    return render_template()
+    return render_template("challenge103.html")
+
+@app.route("/correct")
+def correct():
+    return render_template("correct.html")
+
+@app.route("/trivia", methods=["POST"])
+def question():
+    if request.form.get("answer") == "hearts":
+         return redirect("correct")
+    else:
+        return redirect("/")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=2224)
